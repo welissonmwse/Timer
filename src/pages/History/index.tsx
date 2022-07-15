131b772a5837c5a8dbc7b0bc7d35bd6c859e180e
Welisson Silveira <welissonmwse@gmail.com>
2022-07-15 09:23:31 -0300

    criando componente de Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -19,25 +19,33 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <C.Status statusColor="yellow">Em andamento</C.Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <C.Status statusColor="green">Concluído</C.Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <C.Status statusColor="red">Interrompido</C.Status>
+              </td>
             </tr>
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <C.Status statusColor="green">Concluído</C.Status>
+              </td>
             </tr>
           </tbody>
         </table>
