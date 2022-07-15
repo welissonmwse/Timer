a0438b6286ef5dba0509aa0c010318508e0aa87e
Welisson Silveira <welissonmwse@gmail.com>
2022-07-15 08:58:23 -0300

    finalizando a pagina History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,47 @@
+import * as C from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <C.HistoryContainer>
+      <h1>Meu histórico</h1>
+      <C.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </C.HistoryList>
+    </C.HistoryContainer>
+  )
 }
